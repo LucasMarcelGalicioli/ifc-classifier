3ab6266646d6b1037cf0bf581d7ed0deb974579e
LucasMarcelGalicioli <lucas.galicioli@rogga.com.br>
2025-10-08 13:50:40 -0300

    Initial commit from cookiecutter data science template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projeto para classificar elementos de arquivos IFC usando Machine Learning.',
+    author='Lucas Marcel Galicioli',
+    license='MIT',
+)
