4a5323acb44ea6b3cd19decd7ee95df2c86d3924
LucasMarcelGalicioli <lucas.galicioli@rogga.com.br>
2025-10-24 13:39:05 -0300

    Commit: New_Machine 24_10_2025

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\lucas.galicioli\ifc-classifier\.venv"
+set "VIRTUAL_ENV=C:\Users\lucas.galicioli\Desktop\ifc-classifier\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
